ccff28ae4c2f29e5656d935da6f46728dac5c4e6
HarpiglE <harpigle@gmail.com>
2018-11-26 22:48:38 +0330

    Add a fragment to monitoring information

---
+++ app/src/main/java/com/example/harpigle/happybirthday/BirthdayInformationFragment.java
@@ -0,0 +1,24 @@
+package com.example.harpigle.happybirthday;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BirthdayInformationFragment extends Fragment {
+
+
+    public BirthdayInformationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_birthday_information, container, false);
+    }
+
+}
