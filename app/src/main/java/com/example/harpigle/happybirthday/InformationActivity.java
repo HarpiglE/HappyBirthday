b4d4c75c1db93e5824a6618f9e3ef1fdcec1f291
HarpiglE <harpigle@gmail.com>
2018-12-14 23:05:00 +0330

    Add person counter to info actvity action bar

--- app/src/main/java/com/example/harpigle/happybirthday/InformationActivity.java
+++ app/src/main/java/com/example/harpigle/happybirthday/InformationActivity.java
@@ -1,6 +1,9 @@
 package com.example.harpigle.happybirthday;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,6 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -122,6 +126,9 @@ public class InformationActivity extends AppCompatActivity
     // Deletion dialog callback
     @Override
     public void deletionDone() {
+        // Update action bar
+        invalidateOptionsMenu();
+
         setUpRecyclerView();
     }
 
@@ -140,6 +147,31 @@ public class InformationActivity extends AppCompatActivity
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        setCount(InformationActivity.this, menu, String.valueOf(information.size()));
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    // Set person count budge
+    public void setCount(Context context, Menu menu, String count) {
+        MenuItem menuItem = menu.findItem(R.id.persons_count);
+        LayerDrawable icon = (LayerDrawable) menuItem.getIcon();
+        PersonsCountDrawable badge;
+
+        // Reuse drawable if possible
+        Drawable reuse = icon.findDrawableByLayerId(R.id.persons_count_element);
+        if (reuse != null && reuse instanceof PersonsCountDrawable) {
+            badge = (PersonsCountDrawable) reuse;
+        } else {
+            badge = new PersonsCountDrawable(context);
+        }
+
+        badge.setCount(count);
+        icon.mutate();
+        icon.setDrawableByLayerId(R.id.persons_count_element, badge);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
@@ -151,7 +183,17 @@ public class InformationActivity extends AppCompatActivity
                             new InformationActivityDeletionDialog();
                     clearDialog.show(getSupportFragmentManager(), "CLEAR_DIALOG");
                 }
+                break;
+            case R.id.persons_count:
+                if (information.size() > 0) {
+                    Toast.makeText(
+                            InformationActivity.this,
+                            getString(R.string.persons_count_message, information.size()),
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
